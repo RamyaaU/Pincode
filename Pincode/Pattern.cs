9002e4fe9dbc1b6e78331b73213e166953bd4b8e
Ramya.U <ramyayadav2016@gmail.com>
2020-10-30 21:26:04 +0530

    [Ramya] Add . Added UC_3

---
+++ Pincode/Pattern.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Pincode
+{
+    class Pattern
+    {
+        //pattern representing pincode which consists only digits
+        //alphabets or special characters are not allowed
+        public static string REGEX_PINCODE = "^[1-9][0-9]{5}$";//400088
+
+        /// <summary>
+        /// Validates the pincode.
+        /// </summary>
+        /// <param name="pincode">The pincode.</param>
+        /// <returns></returns>
+        public bool validatePincode(string pincode)
+        {
+            return Regex.IsMatch(pincode, REGEX_PINCODE);
+        }
+    }
+}
