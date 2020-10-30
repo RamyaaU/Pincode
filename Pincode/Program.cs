4eaf0116fc1f9a4fa5f4e9254fc2272ac30a2e42
Ramya.U <ramyayadav2016@gmail.com>
2020-10-30 21:24:40 +0530

    [Ramya] Add . Added UC_2

--- Pincode/Program.cs
+++ Pincode/Program.cs
@@ -7,6 +7,11 @@ namespace Pincode
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Pincode Validation!");
+            Pattern pattern = new Pattern();
+            Console.WriteLine("Enter you pincode");
+            string pincode = Console.ReadLine();
+            bool valid = pattern.validatePincode(pincode);
+            Console.WriteLine(valid);
         }
     }
 }
